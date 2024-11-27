b9ef22507a6fe79dbec3265fd3b51af3c92f22b6
MT988 <125818076+MT988@users.noreply.github.com>
2024-11-27 10:22:46 +0800

    自用，严禁它用！

--- jpext.js
+++ jpext.js
@@ -92,7 +92,7 @@ var rule = {
             let node = html.data;
             VOD = {
                 vod_id: node.id,
-                vod_name: node.title,
+                vod_name: node.title + ' ⭐' + it.score,
                 vod_pic: node.thumbnail,
                 type_name: node.types[0].name,
                 vod_year: node.year.title,
